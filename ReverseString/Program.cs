48ae1cf685e774ec55b0868955e4b35b8ea9a2cc
Angad Baidwan <angad.baidwan@outlook.com>
2023-01-05 17:31:09 -0500

    Add project files.

---
+++ ReverseString/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ReverseString
+{
+    internal class Program
+    {
+        public static string ReverseString(string stringIn)
+        {
+            string reverseString = "";
+            for (int i = stringIn.Length - 1; i >= 0; i--)
+            {
+                reverseString += stringIn[i];
+            }
+            return reverseString;
+        }
+
+        static void Main(string[] args)
+        {
+            string[] test =  
+                {"helloWorld", 
+                "dog", 
+                "tool", 
+                "draw", 
+                "willTheRealSlimShadyPleaseStandUp"};
+
+            foreach (string strInput in test)
+            {
+                Console.WriteLine("String Input: " + strInput);
+                Console.WriteLine(ReverseString(strInput));
+            }
+        }
+    }
+}
